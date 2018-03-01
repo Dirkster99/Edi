e276f286350a456266454c92a185d973c11a503b
Dirkster99 <Dirk.Bahle@gmx.de>
2018-03-01 20:01:46 +0100

    Adjusted 2 minor problems

--- Edi/Edi.Core/View/Pane/PanesStyleSelector.cs
+++ Edi/Edi.Core/View/Pane/PanesStyleSelector.cs
@@ -16,73 +16,71 @@
     /// 
     /// </summary>
     public class PanesStyleSelector : StyleSelector
-  {
-    #region fields
-    private readonly Dictionary<Type, Style> _StyleDirectory = null;
-    #endregion fields
-
-    #region constructor
-    /// <summary>
-    /// Class constructor
-    /// </summary>
-    public PanesStyleSelector()
     {
-    }
-    #endregion constructor
+        #region fields
+        private readonly Dictionary<Type, Style> _StyleDirectory = null;
+        #endregion fields
 
-    #region methods
-    /// <summary>
-    /// Returns a System.Windows.Style based on custom logic.
-    /// </summary>
-    /// <param name="item">The content (usually a viewmodel).</param>
-    /// <param name="container">The element to which the style will be applied.</param>
-    /// <returns>Returns an application-specific style to apply; otherwise, null.</returns>
-    public override System.Windows.Style SelectStyle(object item,
+        #region constructor
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public PanesStyleSelector()
+        {
+            _StyleDirectory = new Dictionary<Type, Style>();
+        }
+        #endregion constructor
+
+        #region methods
+        /// <summary>
+        /// Returns a System.Windows.Style based on custom logic.
+        /// </summary>
+        /// <param name="item">The content (usually a viewmodel).</param>
+        /// <param name="container">The element to which the style will be applied.</param>
+        /// <returns>Returns an application-specific style to apply; otherwise, null.</returns>
+        public override System.Windows.Style SelectStyle(object item,
                                                      System.Windows.DependencyObject container)
-    {
-      if (this._StyleDirectory == null)
-        return null;
+        {
+            if (_StyleDirectory == null)
+                return null;
 
-      if (item == null)
-        return null;
+            if (item == null)
+                return null;
 
-      Style o;
-      Type t = item.GetType();
-      this._StyleDirectory.TryGetValue(t, out o);
+            Style o;
+            Type t = item.GetType();
+            _StyleDirectory.TryGetValue(t, out o);
 
-      if (o != null)
-        return o;
+            if (o != null)
+                return o;
 
-      // Traverse backwards in the inheritance chain to find a mapping there
-      //
-      // https://stackoverflow.com/questions/8699053/how-to-check-if-a-class-inherits-another-class-without-instantiating-it
-      // Lets use .net to check up the inheritance chain to determine
-      // if we can return a style for an inheritated viewmodel instead
-      // of using the direct viewmodel <-> style association.
-      foreach (var vmItem in _StyleDirectory.Keys)
-      {
-          if (t.IsSubclassOf(vmItem) == true)
-          {
-              _StyleDirectory.TryGetValue(vmItem, out o);
-              return o;
-          }
-      }
+            // Traverse backwards in the inheritance chain to find a mapping there
+            //
+            // https://stackoverflow.com/questions/8699053/how-to-check-if-a-class-inherits-another-class-without-instantiating-it
+            // Lets use .net to check up the inheritance chain to determine
+            // if we can return a style for an inheritated viewmodel instead
+            // of using the direct viewmodel <-> style association.
+            foreach (var vmItem in _StyleDirectory.Keys)
+            {
+                if (t.IsSubclassOf(vmItem) == true)
+                {
+                    _StyleDirectory.TryGetValue(vmItem, out o);
+                    return o;
+                }
+            }
 
-      return base.SelectStyle(item, container);
+            return base.SelectStyle(item, container);
+        }
+
+        /// <summary>
+        /// Register a (viewmodel) class type with a <seealso cref="Style"/> for a view.
+        /// </summary>
+        /// <param name="typeOfViewmodel"></param>
+        /// <param name="styleOfView"></param>
+        public void RegisterStyle(Type typeOfViewmodel, Style styleOfView)
+        {
+            _StyleDirectory.Add(typeOfViewmodel, styleOfView);
+        }
+        #endregion methods
     }
-
-    /// <summary>
-    /// Register a (viewmodel) class type with a <seealso cref="Style"/> for a view.
-    /// </summary>
-    /// <param name="typeOfViewmodel"></param>
-    /// <param name="styleOfView"></param>
-    public void RegisterStyle(Type typeOfViewmodel, Style styleOfView)
-    {
-      if (this._StyleDirectory == null)
-        this._StyleDirectory = new Dictionary<Type, Style>();
-
-      this._StyleDirectory.Add(typeOfViewmodel, styleOfView);
-    }
-    #endregion methods
-  }
 }
--- Edi/Edi.Core/View/Pane/PanesTemplateSelector.cs
+++ Edi/Edi.Core/View/Pane/PanesTemplateSelector.cs
@@ -1,65 +1,63 @@
 namespace Edi.Core.View.Pane
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Windows;
-  using System.Windows.Controls;
-
-  /// <summary>
-  /// Data template selector that can be used to select a <seealso cref="DataTemplate"/>
-  /// for view instance based on a viewmodel (class) type.
-  /// </summary>
-  public class PanesTemplateSelector : DataTemplateSelector
-  {
-    #region fields
-    private readonly Dictionary<Type, DataTemplate> _TemplateDirectory = null;
-    #endregion fields
-
-    #region constructor
-    /// <summary>
-    /// Class constructor
-    /// </summary>
-    public PanesTemplateSelector()
-    {
-    }
-    #endregion constructor
-
-    #region methods
-    /// <summary>
-    /// Returns a System.Windows.DataTemplate based on custom logic.
-    /// </summary>
-    /// <param name="item">The data object for which to select the template.</param>
-    /// <param name="container">The data-bound object.</param>
-    /// <returns>Returns a System.Windows.DataTemplate or null. The default value is null.</returns>
-    public override System.Windows.DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
-    {
-      if (_TemplateDirectory == null)
-        return null;
-
-      if (item == null)
-        return null;
-
-      DataTemplate o;
-      _TemplateDirectory.TryGetValue(item.GetType(), out o);
-
-      if (o != null)
-        return o;
-
-      return base.SelectTemplate(item, container);
-    }
+    using System;
+    using System.Collections.Generic;
+    using System.Windows;
+    using System.Windows.Controls;
 
     /// <summary>
-    /// Register a (viewmodel) class type with a <seealso cref="DataTemplate"/> for a view.
+    /// Data template selector that can be used to select a <seealso cref="DataTemplate"/>
+    /// for view instance based on a viewmodel (class) type.
     /// </summary>
-    /// <param name="typeOfViewmodel"></param>
-    /// <param name="view"></param>
-    public void RegisterDataTemplate(Type typeOfViewmodel, DataTemplate view)
+    public class PanesTemplateSelector : DataTemplateSelector
     {
-      if (_TemplateDirectory == null)
-        _TemplateDirectory = new Dictionary<Type, DataTemplate>();
+        #region fields
+        private readonly Dictionary<Type, DataTemplate> _TemplateDirectory = null;
+        #endregion fields
 
-      _TemplateDirectory.Add(typeOfViewmodel, view);
+        #region constructor
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public PanesTemplateSelector()
+        {
+            _TemplateDirectory = new Dictionary<Type, DataTemplate>();
+        }
+        #endregion constructor
+
+        #region methods
+        /// <summary>
+        /// Returns a System.Windows.DataTemplate based on custom logic.
+        /// </summary>
+        /// <param name="item">The data object for which to select the template.</param>
+        /// <param name="container">The data-bound object.</param>
+        /// <returns>Returns a System.Windows.DataTemplate or null. The default value is null.</returns>
+        public override System.Windows.DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
+        {
+            if (_TemplateDirectory == null)
+                return null;
+
+            if (item == null)
+                return null;
+
+            DataTemplate o;
+            _TemplateDirectory.TryGetValue(item.GetType(), out o);
+
+            if (o != null)
+                return o;
+
+            return base.SelectTemplate(item, container);
+        }
+
+        /// <summary>
+        /// Register a (viewmodel) class type with a <seealso cref="DataTemplate"/> for a view.
+        /// </summary>
+        /// <param name="typeOfViewmodel"></param>
+        /// <param name="view"></param>
+        public void RegisterDataTemplate(Type typeOfViewmodel, DataTemplate view)
+        {
+            _TemplateDirectory.Add(typeOfViewmodel, view);
+        }
+        #endregion methods
     }
-    #endregion methods
-  }
 }
