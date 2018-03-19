46d2a28554da5bd4152c18a5a2779e56694d608c
MCCshreyas <shreyasjejurkar123@live.com>
2018-03-20 00:20:35 +0530

    Intended code in Edi.Util project

--- Edi/Edi.Util/ActivateWindow/WindowLister.cs
+++ Edi/Edi.Util/ActivateWindow/WindowLister.cs
@@ -1,112 +1,112 @@
 ﻿namespace Edi.Util.ActivateWindow
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Diagnostics;
-	using System.Runtime.InteropServices;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
 
-	/// <summary>
-	/// This class manages basic information about a process running on the computer.
-	/// 
-	/// Source: http://www.xtremevbtalk.com/showthread.php?t=318187
-	/// </summary>
-	public class WindowInfo
-	{
-		public IntPtr Handle { get; set; }
-		public string Title { get; set; }
-		public string ProcessName { get; set; }
-	}
+    /// <summary>
+    /// This class manages basic information about a process running on the computer.
+    /// 
+    /// Source: http://www.xtremevbtalk.com/showthread.php?t=318187
+    /// </summary>
+    public class WindowInfo
+    {
+        public IntPtr Handle { get; set; }
+        public string Title { get; set; }
+        public string ProcessName { get; set; }
+    }
 
-	/// <summary>
-	/// Source: http://www.xtremevbtalk.com/showthread.php?t=318187
-	/// </summary>
-	public class WindowLister
-	{
-		#region fields
-		protected static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-		#endregion fields
+    /// <summary>
+    /// Source: http://www.xtremevbtalk.com/showthread.php?t=318187
+    /// </summary>
+    public class WindowLister
+    {
+        #region fields
+        protected static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        #endregion fields
 
-		#region methods
-		/// <summary>
-		/// Receive a list of named (or all) processs' running at present on this computer.
-		/// </summary>
-		/// <param name="processName">Name of process to list or null (returns all processs')</param>
-		/// <returns></returns>
-		public WindowInfo[] GetWindows(string processName = null)
-		{
-			List<WindowInfo> windows = new List<WindowInfo>();
+        #region methods
+        /// <summary>
+        /// Receive a list of named (or all) processs' running at present on this computer.
+        /// </summary>
+        /// <param name="processName">Name of process to list or null (returns all processs')</param>
+        /// <returns></returns>
+        public WindowInfo[] GetWindows(string processName = null)
+        {
+            List<WindowInfo> windows = new List<WindowInfo>();
 
-		    var allProcesses = processName == null ? Process.GetProcesses() : Process.GetProcessesByName(processName);
+            var allProcesses = processName == null ? Process.GetProcesses() : Process.GetProcessesByName(processName);
 
-			foreach (Process process in allProcesses)
-			{
-				if (process.MainWindowHandle != IntPtr.Zero)
-				{
-					windows.Add(new WindowInfo()
-					{
-						Handle = process.MainWindowHandle,
-						ProcessName = process.ProcessName,
-						Title = process.MainWindowTitle
-					});
-				}
-			}
+            foreach (Process process in allProcesses)
+            {
+                if (process.MainWindowHandle != IntPtr.Zero)
+                {
+                    windows.Add(new WindowInfo()
+                    {
+                        Handle = process.MainWindowHandle,
+                        ProcessName = process.ProcessName,
+                        Title = process.MainWindowTitle
+                    });
+                }
+            }
 
-			return windows.ToArray();
-		}
+            return windows.ToArray();
+        }
 
-		/// <summary>
-		/// Activate a window that belongs to a process with the given name.
-		/// The Window is given input focus and displayed in the foreground
-		/// (if it is not minimized).
-		/// </summary>
-		/// <param name="procName">Name of process who's Window is to be activated.</param>
-		public static void ActivateMainWindow(string procName)
-		{
-			WindowLister l = new WindowLister();
-			WindowInfo[] w = l.GetWindows(procName);
+        /// <summary>
+        /// Activate a window that belongs to a process with the given name.
+        /// The Window is given input focus and displayed in the foreground
+        /// (if it is not minimized).
+        /// </summary>
+        /// <param name="procName">Name of process who's Window is to be activated.</param>
+        public static void ActivateMainWindow(string procName)
+        {
+            WindowLister l = new WindowLister();
+            WindowInfo[] w = l.GetWindows(procName);
 
-			if (w == null)
-			{
-				logger.Warn("--> Failed to activate Window (w is null).");
-				return;
-			}
+            if (w == null)
+            {
+                logger.Warn("--> Failed to activate Window (w is null).");
+                return;
+            }
 
-			if (w.Length == 0)
-			{
-				logger.Warn("--> Failed to activate Window (w is zero).");
-				return;
-			}
+            if (w.Length == 0)
+            {
+                logger.Warn("--> Failed to activate Window (w is zero).");
+                return;
+            }
 
-			foreach (WindowInfo wi in w)
-			{
-				if (wi.ProcessName == procName)
-				{
-					bool success = NativeMethods.SetForegroundWindow(wi);
+            foreach (WindowInfo wi in w)
+            {
+                if (wi.ProcessName == procName)
+                {
+                    bool success = NativeMethods.SetForegroundWindow(wi);
 
-					if (success == false)
-						logger.Warn("--> Failed to activate Window (success = false).");
-				}
-			}
-		}
-		#endregion methods
-	}
+                    if (success == false)
+                        logger.Warn("--> Failed to activate Window (success = false).");
+                }
+            }
+        }
+        #endregion methods
+    }
 
-	/// <summary>
-	/// Wrapper class for the native user32.dll SetForegroundWindow method.
-	/// </summary>
-	public class NativeMethods
-	{
-		[DllImport("user32.dll")]
-		private static extern bool SetForegroundWindow(IntPtr hWnd);
+    /// <summary>
+    /// Wrapper class for the native user32.dll SetForegroundWindow method.
+    /// </summary>
+    public class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
 
-		/// <summary>
-		/// Wrapper function for the SetForegroundWindow method in the User32.dll.
-		/// </summary>
-		/// <param name="window"></param>
-		/// <returns></returns>
-		public static bool SetForegroundWindow(WindowInfo window)
-		{
-			return SetForegroundWindow(window.Handle);
-		}
-	}
+        /// <summary>
+        /// Wrapper function for the SetForegroundWindow method in the User32.dll.
+        /// </summary>
+        /// <param name="window"></param>
+        /// <returns></returns>
+        public static bool SetForegroundWindow(WindowInfo window)
+        {
+            return SetForegroundWindow(window.Handle);
+        }
+    }
 }
--- Edi/Edi.Util/SingletonApplicationEnforcer.cs
+++ Edi/Edi.Util/SingletonApplicationEnforcer.cs
@@ -1,14 +1,14 @@
 ﻿namespace Edi.Util
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Diagnostics;
-	using System.IO;
-	using System.IO.MemoryMappedFiles;
-	using System.Threading;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.IO.MemoryMappedFiles;
+    using System.Threading;
 
-	#region File and License Information
-	/*
+    #region File and License Information
+    /*
 <File>
 	<Copyright>Copyright © 2010, Daniel Vaughan. All rights reserved.</Copyright>
 	<License>
@@ -38,73 +38,74 @@
 	<CreationDate>2010-08-01 18:23:33Z</CreationDate>
 </File>
 */
-	#endregion
+    #endregion
 
-	/// <summary>
-	/// This class allows restricting the number of executables in execution, to one.
-	/// </summary>
-	public sealed class SingletonApplicationEnforcer
-	{
-		#region fields
-		static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    /// <summary>
+    /// This class allows restricting the number of executables in execution, to one.
+    /// </summary>
+    public sealed class SingletonApplicationEnforcer
+    {
+        #region fields
+        static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-		private readonly Action<IEnumerable<string>> processArgsFunc;
-		private readonly Action<string> processActivateFunc;
-		private readonly string applicationId;
+        private readonly Action<IEnumerable<string>> processArgsFunc;
+        private readonly Action<string> processActivateFunc;
+        private readonly string applicationId;
 
-		Thread thread;
-		Thread windowActivationThread;
-		string argDelimiter = "_;;_";
-		#endregion fields
+        Thread thread;
+        Thread windowActivationThread;
+        string argDelimiter = "_;;_";
+        #endregion fields
 
-		#region properties
-		/// <summary>
-		/// Gets or sets the string that is used to join 
-		/// the string array of arguments in memory.
-		/// </summary>
-		/// <value>The arg delimeter.</value>
-		public string ArgDelimeter
-		{
-			get => argDelimiter;
-		    set => argDelimiter = value;
-		}
+        #region properties
+        /// <summary>
+        /// Gets or sets the string that is used to join 
+        /// the string array of arguments in memory.
+        /// </summary>
+        /// <value>The arg delimeter.</value>
+        public string ArgDelimeter
+        {
+            get => argDelimiter;
+            set => argDelimiter = value;
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="SingletonApplicationEnforcer"/> class.
-		/// </summary>
-		/// <param name="processArgsFunc">A handler for processing command line args 
-		/// when they are received from another application instance.</param>
-		/// <param name="applicationId">The application id used 
-		/// for naming the <seealso cref="EventWaitHandle"/>.</param>
-		public SingletonApplicationEnforcer(Action<IEnumerable<string>> processArgsFunc, Action<string> processArgsFunc1,
-																				string applicationId = "DisciplesRock")
-		{
-		    this.processArgsFunc = processArgsFunc ?? throw new ArgumentNullException(nameof(processArgsFunc));
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SingletonApplicationEnforcer"/> class.
+        /// </summary>
+        /// <param name="processArgsFunc">A handler for processing command line args 
+        /// when they are received from another application instance.</param>
+        /// <param name="processArgsFunc1"></param>
+        /// <param name="applicationId">The application id used 
+        /// for naming the <seealso cref="EventWaitHandle"/>.</param>
+        public SingletonApplicationEnforcer(Action<IEnumerable<string>> processArgsFunc, Action<string> processArgsFunc1,
+                                                                                string applicationId = "DisciplesRock")
+        {
+            this.processArgsFunc = processArgsFunc ?? throw new ArgumentNullException(nameof(processArgsFunc));
 
-		    processActivateFunc = processArgsFunc1;
-			this.applicationId = applicationId;
-		}
-		#endregion properties
+            processActivateFunc = processArgsFunc1;
+            this.applicationId = applicationId;
+        }
+        #endregion properties
 
-		#region methods
-		/// <summary>
-		/// Determines if this application instance is not the singleton instance.
-		/// If this application is not the singleton, then it should exit.
-		/// </summary>
-		/// <returns><c>true</c> if the application should shutdown, 
-		/// otherwise <c>false</c>.</returns>
-		public bool ShouldApplicationExit()
-		{
+        #region methods
+        /// <summary>
+        /// Determines if this application instance is not the singleton instance.
+        /// If this application is not the singleton, then it should exit.
+        /// </summary>
+        /// <returns><c>true</c> if the application should shutdown, 
+        /// otherwise <c>false</c>.</returns>
+        public bool ShouldApplicationExit()
+        {
             string argsWaitHandleName = "ArgsWaitHandle_" + applicationId;
             string memoryFileName = "ArgFile_" + applicationId;
 
-		    EventWaitHandle argsWaitHandle = new EventWaitHandle(
-				false, EventResetMode.AutoReset, argsWaitHandleName, out var createdNew);
+            EventWaitHandle argsWaitHandle = new EventWaitHandle(
+                false, EventResetMode.AutoReset, argsWaitHandleName, out var createdNew);
 
-			GC.KeepAlive(argsWaitHandle);
-             
-			if (createdNew)
-			{
+            GC.KeepAlive(argsWaitHandle);
+
+            if (createdNew)
+            {
                 /* This is the main, or singleton application. 
 				 * A thread is created to service the MemoryMappedFile. 
 				 * We repeatedly examine this file each time the argsWaitHandle 
@@ -148,49 +149,49 @@
                     IsBackground = true
                 };
                 thread.Start();
-			}
-			else
-			{
-				try
-				{
-					windowActivationThread = new Thread(() =>
-					{
-						try
-						{
-							processActivateFunc(this.applicationId);
-						}
-						catch (Exception ex)
-						{
-							logger.Error("Error activating window", ex);
-						}
-					});
+            }
+            else
+            {
+                try
+                {
+                    windowActivationThread = new Thread(() =>
+                    {
+                        try
+                        {
+                            processActivateFunc(this.applicationId);
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.Error("Error activating window", ex);
+                        }
+                    });
 
-					windowActivationThread.Start();
+                    windowActivationThread.Start();
 
-					/* Non singleton application instance. 
+                    /* Non singleton application instance. 
 					 * Should exit, after passing command line args to singleton process, 
 					 * via the MemoryMappedFile. */
-					using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting(memoryFileName))
-					{
-						using (MemoryMappedViewStream stream = mmf.CreateViewStream())
-						{
-							var writer = new BinaryWriter(stream);
-							string[] args = Environment.GetCommandLineArgs();
-							string joined = string.Join(argDelimiter, args);
-							writer.Write(joined);
-						}
-					}
-					argsWaitHandle.Set();
+                    using (MemoryMappedFile mmf = MemoryMappedFile.OpenExisting(memoryFileName))
+                    {
+                        using (MemoryMappedViewStream stream = mmf.CreateViewStream())
+                        {
+                            var writer = new BinaryWriter(stream);
+                            string[] args = Environment.GetCommandLineArgs();
+                            string joined = string.Join(argDelimiter, args);
+                            writer.Write(joined);
+                        }
+                    }
+                    argsWaitHandle.Set();
 
-				}
-				catch (Exception ex)
-				{
-					logger.Error("Error on OpenExisting memory mapped file", ex);
-				}
-			}
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("Error on OpenExisting memory mapped file", ex);
+                }
+            }
 
-			return !createdNew;
-		}
-		#endregion properties
-	}
+            return !createdNew;
+        }
+        #endregion properties
+    }
 }
