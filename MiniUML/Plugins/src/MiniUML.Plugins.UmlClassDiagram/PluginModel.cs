660b9e833d92bf05bbc1a9df62ff44a01a226a79
Dirkster99 <Dirk.Bahle@gmx.de>
2018-07-05 22:34:48 +0200

    Fixed minor problem in Anchor Point (was  always visible for mouseover)

--- MiniUML/Plugins/src/MiniUML.Plugins.UmlClassDiagram/PluginModel.cs
+++ MiniUML/Plugins/src/MiniUML.Plugins.UmlClassDiagram/PluginModel.cs
@@ -1,78 +1,77 @@
 namespace MiniUML.Plugins.UmlClassDiagram
 {
-  using System.Windows;
-  using MiniUML.Model.Model;
-  using MiniUML.Model.ViewModels;
-  using MiniUML.Model.ViewModels.Document;
-  using MiniUML.Plugins.UmlClassDiagram.Converter;
+    using System.Windows;
+    using MiniUML.Model.Model;
+    using MiniUML.Model.ViewModels.Document;
+    using MiniUML.Plugins.UmlClassDiagram.Converter;
 
-  /// <summary>
-  /// Manage a model for this plug-in.
-  /// </summary>
-  public class PluginModel : MiniUML.Model.PluginModelBase
-  {
-    #region field
     /// <summary>
-    /// Unique plugin name to identify 1 MiniUml plugin among many others.
-    /// Changing this requires changing strings in all applications that load this plug-in.
-    /// So, never change this string (ever).
+    /// Manage a model for this plug-in.
     /// </summary>
-    public const string ModelName = "UMLClassDiagram";
-
-    private FrameworkElement mPluginView;
-
-    private UmlTypeToStringConverter mShapeConverter = null;
-    #endregion field
-
-    #region constructor
-    /// <summary>
-    /// Class constructor from document viewmodel
-    /// </summary>
-    /// <param name="windowViewModel"></param>
-    public PluginModel(IMiniUMLDocument windowViewModel)
+    public class PluginModel : MiniUML.Model.PluginModelBase
     {
-      this.mShapeConverter = new UmlTypeToStringConverter();
-      this.mPluginView = new PluginView();
-      this.mPluginView.DataContext = new PluginViewModel(windowViewModel);
-    }
-    #endregion constructor
+        #region field
+        /// <summary>
+        /// Unique plugin name to identify 1 MiniUml plugin among many others.
+        /// Changing this requires changing strings in all applications that load this plug-in.
+        /// So, never change this string (ever).
+        /// </summary>
+        public const string ModelName = "UMLClassDiagram";
 
-    #region properties
-    /// <summary>
-    /// Get a name for the plug-in model. Each plug-in must have a unique name.
-    /// </summary>
-    public override string Name
-    {
-      get
-      {
-        return PluginModel.ModelName;
-      }
-    }
+        private FrameworkElement _PluginView;
 
-    /// <summary>
-    /// Get a view for this plug-in.
-    /// </summary>
-    public override FrameworkElement View
-    {
-      get
-      {
-        return this.mPluginView;
-      }
-    }
+        private UmlTypeToStringConverter _ShapeConverter = null;
+        #endregion field
 
-    /// <summary>
-    /// Get shape converter from unique string to shape viewmodel instance.
-    /// </summary>
-    public override UmlTypeToStringConverterBase ShapeConverter
-    {
-      get
-      {
-        return this.mShapeConverter;
-      }
-    }
-    #endregion properties
+        #region constructor
+        /// <summary>
+        /// Class constructor from document viewmodel
+        /// </summary>
+        /// <param name="windowViewModel"></param>
+        public PluginModel(IMiniUMLDocument windowViewModel)
+        {
+            _ShapeConverter = new UmlTypeToStringConverter();
+            _PluginView = new PluginView();
+            _PluginView.DataContext = new PluginViewModel(windowViewModel);
+        }
+        #endregion constructor
 
-    #region methods
-    #endregion methods
-  }
+        #region properties
+        /// <summary>
+        /// Get a name for the plug-in model. Each plug-in must have a unique name.
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return PluginModel.ModelName;
+            }
+        }
+
+        /// <summary>
+        /// Get a view for this plug-in.
+        /// </summary>
+        public override FrameworkElement View
+        {
+            get
+            {
+                return _PluginView;
+            }
+        }
+
+        /// <summary>
+        /// Get shape converter from unique string to shape viewmodel instance.
+        /// </summary>
+        public override UmlTypeToStringConverterBase ShapeConverter
+        {
+            get
+            {
+                return _ShapeConverter;
+            }
+        }
+        #endregion properties
+
+        #region methods
+        #endregion methods
+    }
 }
